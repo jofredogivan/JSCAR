99e5f30cd4ae531a20e6fc3fb8f3c3a538f1a1e7
jofredogivan <jofredogivan@gmail.com>
2026-05-01 15:38:05 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,24 @@
-const CACHE_NAME = 'japan-security-v3';
+const CACHE_NAME = 'japan-v4';
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icons/icon-192x192.png',
-  './icons/icon-512x512.png'
+  '/JSCAR/',
+  '/JSCAR/index.html',
+  '/JSCAR/manifest.json',
+  '/JSCAR/icons/icon-192x192.png',
+  '/JSCAR/icons/icon-512x512.png'
 ];
 
-// Instala o Service Worker e armazena os arquivos no cache
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-// Responde às requisições usando o cache quando offline
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
