1d248b8ea401011a79ebb8c384e9b94156d44d30
jofredogivan <jofredogivan@gmail.com>
2026-05-01 14:34:26 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,22 @@
-const CACHE_NAME = 'japan-security-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/vistoria.html',
-  '/dashboard.html',
-  '/css/style.css',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+const CACHE_NAME = 'japan-security-v2';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png'
 ];
 
-// Instalação: Salva os arquivos essenciais no cache
-self.addEventListener('install', (event) => {
+// Instala o Service Worker e armazena os arquivos no cache
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
-  self.skipWaiting();
-});
-
-// Ativação: Limpa caches antigos se você atualizar a versão
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Intercepta as requisições: Tenta carregar do cache, se não tiver, busca na rede
-self.addEventListener('fetch', (event) => {
+// Responde às requisições usando o cache quando offline
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
